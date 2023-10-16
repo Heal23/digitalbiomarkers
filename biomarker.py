e5666da7cff6636895667ad3fc2b6f363fb9ad75
Heal23 <hsaine.elali@gmail.com>
2023-10-16 23:30:14 +0200

    version 1

--- biomarker.py
+++ biomarker.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
-import mne
 import matplotlib.pyplot as plt
 from tabulate import tabulate
 import ast  # For safely evaluating the string
-
 
 # Load the CSV files
 long_blink_data = pd.read_csv('C:/dev/digitalbiomarkers/EEG-data/LongBlink.csv')
@@ -18,29 +16,6 @@ print(tabulate(long_blink_data.head(), headers='keys', tablefmt='grid'))
 print("\nTable for ShortBlink.csv:")
 print(tabulate(short_blink_data.head(), headers='keys', tablefmt='grid'))
 
-
-"""# Statistical Summary
-print("Statistics for LongBlink.csv:")
-print(long_blink_data.describe())
-print("\nStatistics for ShortBlink.csv:")
-print(short_blink_data.describe())
-
-# Missing values
-print("Missing values in LongBlink.csv:", long_blink_data.isnull().sum())
-print("Missing values in ShortBlink.csv:", short_blink_data.isnull().sum())
-
-
-# Visualise distribution
-plt.figure(figsize=(10,5))
-long_blink_data['data'].apply(ast.literal_eval).explode().astype(float).hist(alpha=0.5, label='Long Blink')
-short_blink_data['data'].apply(ast.literal_eval).explode().astype(float).hist(alpha=0.5, label='Short Blink')
-plt.legend()
-plt.xlabel('EEG Value')
-plt.ylabel('Frequency')
-plt.title('Distribution of EEG Values')
-plt.show()"""
-
-
 # Convert string representations to actual lists
 long_blink_data['data'] = long_blink_data['data'].apply(ast.literal_eval)
 short_blink_data['data'] = short_blink_data['data'].apply(ast.literal_eval)
@@ -53,7 +28,38 @@ short_blink_values = [value for sublist in short_blink_data['data'].tolist() for
 long_blink_sessions = [long_blink_values[i:i + 510] for i in range(0, len(long_blink_values), 510)]
 short_blink_sessions = [short_blink_values[i:i + 510] for i in range(0, len(short_blink_values), 510)]
 
-# Plot EEG data on a single figure with subplots
+# 1.2 Data Description
+
+# Compute Average amplitude and standard deviation
+avg_amplitudes_long = np.mean(long_blink_sessions, axis=1)
+avg_amplitudes_short = np.mean(short_blink_sessions, axis=1)
+
+std_devs_long = np.std(long_blink_sessions, axis=1)
+std_devs_short = np.std(short_blink_sessions, axis=1)
+
+print(f"Average amplitude for Long Blink sessions: {np.mean(avg_amplitudes_long):.2f}")
+print(f"Average amplitude for Short Blink sessions: {np.mean(avg_amplitudes_short):.2f}")
+print(f"Standard deviation for Long Blink sessions: {np.mean(std_devs_long):.2f}")
+print(f"Standard deviation for Short Blink sessions: {np.mean(std_devs_short):.2f}")
+
+# Frequency distribution (Fourier Transform) for the first session as an example
+sampling_rate = 250
+frequencies = np.fft.rfftfreq(510, d=1./sampling_rate)  # For a session of length 510
+fft_values_long = np.fft.rfft(long_blink_sessions[0])
+fft_values_short = np.fft.rfft(short_blink_sessions[0])
+
+plt.figure(figsize=(10, 5))
+plt.plot(frequencies, np.abs(fft_values_long)**2, label='Long Blink')
+plt.plot(frequencies, np.abs(fft_values_short)**2, label='Short Blink')
+plt.title("Power Spectral Density of the First Session")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Power")
+plt.xlim([0, 60])
+plt.legend()
+plt.show()
+
+# Continue with your plotting function
+
 def plot_eeg_sessions_on_single_plot(sessions, title_prefix):
     # Create a large figure with multiple subplots
     fig, axs = plt.subplots(len(sessions), 1, figsize=(10, len(sessions) * 1.5))
@@ -73,9 +79,8 @@ def plot_eeg_sessions_on_single_plot(sessions, title_prefix):
     # Adjust spacing between plots
     plt.tight_layout()
 
-# Plot sessions for LongBlink.csv and ShortBlink.csv, only the first 10 sessions
-plot_eeg_sessions_on_single_plot(long_blink_sessions[:10], 'Long Blink')
-plot_eeg_sessions_on_single_plot(short_blink_sessions[:10], 'Short Blink')
+# Plot sessions for LongBlink.csv and ShortBlink.csv, only the first 20 sessions
+plot_eeg_sessions_on_single_plot(long_blink_sessions[:20], 'Long Blink')
+plot_eeg_sessions_on_single_plot(short_blink_sessions[:20], 'Short Blink')
 
 plt.show()
-
